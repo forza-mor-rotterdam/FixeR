cde8ba7d42fd03849c8457b1a8dcb97b84e79fbf
Remco Hoff <remco@code010.nl>
2023-10-26 12:10:31 +0200

    New labels for Rol

--- app/apps/context/forms.py
+++ app/apps/context/forms.py
@@ -54,3 +54,22 @@ class ContextAanmakenForm(ContextAanpassenForm):
             "filters",
             "template",
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields[
+            "naam"
+        ].help_text = "Omschrijf de rol zo concreet mogelijk, bijvoorbeeld ‘Teamleider Inzameling Noord’."
+        self.fields[
+            "taaktypes"
+        ].help_text = "De taaktypes die hier worden geselecteerd worden getoond aan gebruikers met deze rol."
+        self.fields[
+            "filters"
+        ].help_text = (
+            "De hier geselecteerde opties worden getoond in het Filter-menu van FixeR."
+        )
+        self.fields[
+            "template"
+        ].help_text = "Ieder sjabloon toont andere informatie. Het ‘Standaard’ sjabloon voldoet voor de meeste afdelingen."
+        self.fields["taaktypes"].label = "Met welke taaktypes werkt deze rol?"
+        self.fields["filters"].label = "Welke filters zijn relevant voor deze rol?"
