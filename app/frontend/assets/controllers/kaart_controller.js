f374f223f3d5d0b9c4566689657ad63164dc131e
Jorrit Strikwerda <jippiejorrit@gmail.com>
2023-12-11 15:26:37 +0100

    Updated taak on map to show Details | Navigeer anchors.

--- app/frontend/assets/controllers/kaart_controller.js
+++ app/frontend/assets/controllers/kaart_controller.js
@@ -256,12 +256,13 @@ export default class extends Controller {
           taakId: taakId,
         });
         const paragraphDistance = `<p>Afstand: <span data-incidentlist-target="taakAfstand" data-latitude="${lat}" data-longitude="${long}"></span> meter</p>`;
-        const anchorDetail = `<a href="/taak/${taakId}" target="_top" aria-label="Bekijk taak ${taakId}">Bekijk details</a>`;
+        const anchorDetail = `<a href="/taak/${taakId}" target="_top" aria-label="Bekijk taak ${taakId}">Details</a>`;
+        const anchorNavigeer = `<span class="link" data-action="click->kaart#makeRoute" data-kaart-lat-param="${lat}" data-kaart-long-param="${long}" target="_top" aria-label="Navigeer naar taak ${taakId}">Navigeren</span>`;
         // let popupContent = `<div class="container__content"><a href="/taak/${taakId}" target="_top" aria-label="Bekijk taak ${taakId}">${adres}</a><p>${omschrijving}</p>${paragraphDistance}</div>`
-        let popupContent = `<div class="container__content"><span class="link" data-action="click->kaart#makeRoute" data-kaart-lat-param="${lat}" data-kaart-long-param="${long}" aria-label="Bekijk taak ${taakId}">${adres}</span><p>${omschrijving}</p>${paragraphDistance}${anchorDetail}</div>`;
+        let popupContent = `<div class="container__content"><h5 class="h5">${adres}</h5><p>${omschrijving}</p>${paragraphDistance}${anchorDetail} | ${anchorNavigeer}</div>`;
         if (afbeelding) {
           // popupContent = `<div class="container__image"><img src=${afbeelding}></div><div class="container__content"><a href="/taak/${taakId}" target="_top" aria-label="Bekijk taak ${taakId}">${adres}</a><p>${omschrijving}</p>${paragraphDistance}</div>`
-          popupContent = `<div class="container__image"><img src=${afbeelding}></div><div class="container__content"><span class="link" data-action="click->kaart#makeRoute" data-kaart-lat-param="${lat}" data-kaart-long-param="${long}" aria-label="Bekijk taak ${taakId}">${adres}</span><p>${omschrijving}</p>${paragraphDistance}${anchorDetail}</div>`;
+          popupContent = `<div class="container__image"><img src=${afbeelding}></div><div class="container__content"><h5 class="no-margin">${adres}</h5><p>${omschrijving}</p>${paragraphDistance}${anchorDetail} | ${anchorNavigeer}</div>`;
         }
         marker.bindPopup(popupContent);
 
