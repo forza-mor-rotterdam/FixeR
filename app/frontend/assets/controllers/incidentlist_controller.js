3da6152a43b5887ef4e2743d0220cbed434da8bc
Remco Hoff <remco@code010.nl>
2024-03-18 11:10:31 +0100

    Styling responsive search

--- app/frontend/assets/controllers/incidentlist_controller.js
+++ app/frontend/assets/controllers/incidentlist_controller.js
@@ -2,6 +2,7 @@ import { Controller } from '@hotwired/stimulus'
 import L from 'leaflet'
 
 let showSortingContainer = false
+let showSearchContainer = false
 let sortDirectionReversed = false
 let currentPosition = null
 
@@ -9,6 +10,7 @@ export default class extends Controller {
   static outlets = ['kaart']
   static targets = [
     'sorting',
+    'search',
     'toggleMapView',
     'taakAfstand',
     'taakItem',
@@ -170,5 +172,12 @@ export default class extends Controller {
     self.sortingTarget.classList.toggle('show-vertical')
     showSortingContainer = !showSortingContainer
     sortDirectionReversed = sortDirectionReversed === undefined ? false : true
+  }
+
+  onToggleSearchContainer() {
+    let self = this
+    self.searchTarget.classList.toggle('hidden-vertical')
+    self.searchTarget.classList.toggle('show-vertical')
+    showSearchContainer = !showSearchContainer
   }
 }
