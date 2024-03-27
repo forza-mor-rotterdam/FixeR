332228604065c40ecb92d1818d103e45895e7861
Remco Hoff <remco@code010.nl>
2024-03-27 08:51:58 +0100

    Refactoring image for pinch

--- app/frontend/assets/controllers/detail_controller.js
+++ app/frontend/assets/controllers/detail_controller.js
@@ -164,6 +164,12 @@ export default class extends Controller {
         self.positionWatchSuccess(e.detail.position)
       })
     }
+
+    document.querySelectorAll('.container__image').forEach((element) => {
+      console.log('this', this)
+      console.log('element', element)
+      self.pinchZoom(element)
+    })
   }
 
   connect() {
@@ -454,7 +460,7 @@ export default class extends Controller {
   }
 
   showImage() {
-    this.selectedImageModalTarget.style.backgroundImage = `url('${this.urlPrefixValue}${imagesList[selectedImageIndex]}')`
+    this.selectedImageModalTarget.src = `${this.urlPrefixValue}${imagesList[selectedImageIndex]}`
     this.showHideImageNavigation()
     this.imageCounterTarget.textContent = `Foto ${selectedImageIndex + 1} van ${imagesList.length}`
     this.imageScrollInView(selectedImageIndex) //image in detailpage
@@ -474,9 +480,9 @@ export default class extends Controller {
   }
 
   showLarge() {
+    console.log('thisssss', this)
     if (fullSizeImageContainer.classList.contains('fullSize')) {
       fullSizeImageContainer.classList.remove('fullSize')
-      fullSizeImageContainer.style.backgroundPosition = '50% 50%'
       window.removeEventListener('mousemove', this.getRelativeCoordinates, true)
     } else {
       fullSizeImageContainer.classList.add('fullSize')
@@ -510,5 +516,68 @@ export default class extends Controller {
     document.body.classList.add('show-modal')
 
     this.showImage()
+  }
+
+  pinchZoom(imageElement) {
+    let imageElementScale = 1
+    let start = {}
+    console.log('pinchZoom')
+    // Calculate distance between two fingers
+    const distance = (event) => {
+      const dist = Math.hypot(
+        event.touches[0].pageX - event.touches[1].pageX,
+        event.touches[0].pageY - event.touches[1].pageY
+      )
+      return dist
+    }
+
+    imageElement.addEventListener('touchstart', (event) => {
+      console.log('touchstart')
+      if (event.touches.length === 2) {
+        event.preventDefault() // Prevent page scroll
+        console.log('event.touches.length === 2')
+        // Calculate where the fingers have started on the X and Y axis
+        start.x = (event.touches[0].pageX + event.touches[1].pageX) / 2
+        start.y = (event.touches[0].pageY + event.touches[1].pageY) / 2
+        start.distance = distance(event)
+      }
+    })
+
+    imageElement.addEventListener('touchmove', (event) => {
+      console.log('touchmove')
+      if (event.touches.length === 2) {
+        console.log('event.touches.length === 2')
+        event.preventDefault() // Prevent page scroll
+
+        // Safari provides event.scale as two fingers move on the screen
+        // For other browsers just calculate the scale manually
+        let scale
+        if (event.scale) {
+          scale = event.scale
+        } else {
+          const deltaDistance = distance(event)
+          scale = deltaDistance / start.distance
+        }
+        imageElementScale = Math.min(Math.max(1, scale), 4)
+
+        // Calculate how much the fingers have moved on the X and Y axis
+        const deltaX = ((event.touches[0].pageX + event.touches[1].pageX) / 2 - start.x) * 2 // x2 for accelarated movement
+        const deltaY = ((event.touches[0].pageY + event.touches[1].pageY) / 2 - start.y) * 2 // x2 for accelarated movement
+
+        // Transform the image to make it grow and move with fingers
+        const transform = `translate3d(${deltaX}px, ${deltaY}px, 0) scale(${imageElementScale})`
+        imageElement.style.transform = transform
+        imageElement.style.WebkitTransform = transform
+        imageElement.style.zIndex = '9999'
+      }
+    })
+
+    imageElement.addEventListener('touchend', () => {
+      console.log('touchend')
+      // Reset image to it's original format
+      imageElement.style.transform = ''
+      imageElement.style.WebkitTransform = ''
+      imageElement.style.zIndex = ''
+    })
   }
 }
