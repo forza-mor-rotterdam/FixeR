abd2444386620703ac99d87fc33fe92971305a1b
Maurice Guikema <maurice@mgui.nl>
2024-04-08 14:48:09 +0200

    logging with response text

--- app/apps/main/views.py
+++ app/apps/main/views.py
@@ -461,6 +461,10 @@ def taak_toewijzen(request, id):
                 gebruiker=request.user.email,
                 uitvoerder=form.cleaned_data.get("uitvoerder"),
             )
+            if taak_status_aanpassen_response.status_code != 200:
+                logger.error(
+                    f"taak_toewijzen taak_status_aanpassen: status_code={taak_status_aanpassen_response.status_code}, taak_id={id}, repsonse_text={taak_status_aanpassen_response.text}"
+                )
             if taak_status_aanpassen_response.status_code == 200:
                 return render(
                     request,
@@ -493,6 +497,10 @@ def taak_toewijzing_intrekken(request, id):
                 status="openstaand",
                 gebruiker=request.user.email,
             )
+            if taak_status_aanpassen_response.status_code != 200:
+                logger.error(
+                    f"taak_toewijzing_intrekken taak_status_aanpassen: status_code={taak_status_aanpassen_response.status_code}, taak_id={id}, repsonse_text={taak_status_aanpassen_response.text}"
+                )
             if taak_status_aanpassen_response.status_code == 200:
                 return render(
                     request,
@@ -581,7 +589,7 @@ def incident_modal_handle(request, id):
             )
             if taak_status_aanpassen_response.status_code != 200:
                 logger.error(
-                    f"taak_status_aanpassen: status code: {taak_status_aanpassen_response.status_code}, taak id: {id}"
+                    f"incident_modal_handle taak_status_aanpassen: status_code={taak_status_aanpassen_response.status_code}, taak_id={id}, repsonse_text={taak_status_aanpassen_response.text}"
                 )
 
             # Aanmaken extra taken
@@ -612,7 +620,7 @@ def incident_modal_handle(request, id):
                         )
                         if taak_aanmaken_response.status_code != 200:
                             logger.error(
-                                f"taak_aanmaken: status code: {taak_aanmaken_response.status_code}, taak id: {id}, text: {taak_aanmaken_response.text}"
+                                f"incident_modal_handle taak_aanmaken: status code: {taak_aanmaken_response.status_code}, taak id: {id}, text: {taak_aanmaken_response.text}"
                             )
             return redirect("taken")
         else:
