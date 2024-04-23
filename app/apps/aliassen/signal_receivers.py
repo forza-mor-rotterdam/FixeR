09003b50617935d57b9617d9a2882ae04806fa8d
Maurice Guikema <maurice@mgui.nl>
2024-04-23 13:08:01 +0200

    disable default melding validation

--- app/apps/aliassen/signal_receivers.py
+++ app/apps/aliassen/signal_receivers.py
@@ -1,10 +1,9 @@
-from apps.aliassen.models import BijlageAlias, MeldingAlias
+from apps.aliassen.models import BijlageAlias
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 
 @receiver(pre_save, sender=BijlageAlias, dispatch_uid="ophalen_bijlagealias_data")
-@receiver(pre_save, sender=MeldingAlias, dispatch_uid="ophalen_meldingalias_data")
 def ophalen_alias_data(sender, instance, **kwargs):
     if kwargs.get("raw"):
         return
