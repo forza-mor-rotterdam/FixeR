1a773c3be803d6a2e618e09014d58792b104fc5d
Remco Hoff <remco@code010.nl>
2024-05-29 18:02:10 +0200

    Styling Taken, added selectAll

--- app/frontend/assets/controllers/onboarding_controller.js
+++ app/frontend/assets/controllers/onboarding_controller.js
@@ -5,9 +5,14 @@ export default class extends Controller {
     dateObject: String,
   }
 
-  //   static targets = ['']
+  static targets = ['takenLijst']
 
-  connect() {
-    console.log('connecting onboarding controller')
+  selectAll(e) {
+    e.preventDefault()
+    const checkList = Array.from(this.takenLijstTarget.querySelectorAll('input.form-check-input'))
+    const doCheck = e.params.filterType === 'all'
+    checkList.forEach((element) => {
+      element.checked = doCheck
+    })
   }
 }
