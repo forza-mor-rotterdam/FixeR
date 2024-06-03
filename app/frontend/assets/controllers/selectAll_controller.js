936db82c198500dfa67d88b3aa02bf902a16f4e8
Remco Hoff <remco@code010.nl>
2024-06-03 14:21:21 +0200

    Update selectAll_controller.js

--- app/frontend/assets/controllers/selectAll_controller.js
+++ app/frontend/assets/controllers/selectAll_controller.js
@@ -8,11 +8,13 @@ export default class extends Controller {
   selectAll(e) {
     e.preventDefault()
     const checkList = Array.from(
-      e.target.closest('.form-row').querySelectorAll('input[type="checkbox"]')
+      e.target
+        .closest('.form-row')
+        .querySelectorAll('li:not([style*="display:none"]):not([style*="display: none"]')
     )
     const doCheck = e.params.filterType === 'all'
     checkList.forEach((element) => {
-      element.checked = doCheck
+      element.querySelector('input[type="checkbox"]').checked = doCheck
     })
   }
 }
