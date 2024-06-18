30ea50035ace21a309985598d53162b319cbed09
Remco Hoff <remco@code010.nl>
2024-06-18 16:50:20 +0200

    Update views.py

--- app/apps/main/views.py
+++ app/apps/main/views.py
@@ -124,10 +124,10 @@ def navigeer(request, lat, long):
 # Verander hier de instellingen voor de nieuwe homepagina.
 # @login_required
 def root(request):
-    # if request.user.has_perms(["authorisatie.taken_lijst_bekijken"]):
-    #     return redirect(reverse("taken"), False)
-    # if request.user.has_perms(["authorisatie.beheer_bekijken"]):
-    #     return redirect(reverse("beheer"), False)
+    if request.user.has_perms(["authorisatie.taken_lijst_bekijken"]):
+        return redirect(reverse("taken"), False)
+    if request.user.has_perms(["authorisatie.beheer_bekijken"]):
+        return redirect(reverse("beheer"), False)
     return render(
         request,
         "home.html",
