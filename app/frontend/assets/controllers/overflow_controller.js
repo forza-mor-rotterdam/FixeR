0a485640646656afc15b6854d053b6168cd64e16
Remco Hoff <remco@code010.nl>
2024-06-27 15:20:40 +0200

    Styling and scripting overflow in alert

---
+++ app/frontend/assets/controllers/overflow_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  connect() {
+    const wrapper = this.element.querySelector('.container__overflow').closest('.wrapper')
+    const container = this.element.querySelector('.container__overflow')
+    const content = this.element.querySelector('.content__overflow')
+
+    if (content.scrollHeight > container.clientHeight) {
+      this.element.closest('.alert').classList.add('has-overflow')
+      const label = document.createElement('span')
+      label.classList.add('toggle')
+      label.textContent = 'Lees meer'
+
+      this.element.insertBefore(label, container.nextElementSibling)
+
+      this.element.addEventListener('click', function () {
+        wrapper.classList.toggle('show')
+        if (label.textContent == 'Lees meer') {
+          label.textContent = 'Lees minder'
+        } else {
+          label.textContent = 'Lees meer'
+        }
+      })
+    }
+  }
+}
