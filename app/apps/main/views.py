d695822069a77034ee870958de43257d1c9e8eae
Jorrit Strikwerda <j.strikwerda@alteza.nl>
2024-07-30 11:09:42 +0200

    Add voltooid_met_feedback, namechange.

--- app/apps/main/views.py
+++ app/apps/main/views.py
@@ -632,7 +632,7 @@ def taak_afhandelen(request, id):
     )
     volgende_taaktypes = []
     actieve_vervolg_taken = []
-    if taak.taakstatus.naam in ["voltooid", "voltooid_met_feedback"]:
+    if taak.taakstatus.naam == "voltooid":
         messages.warning(request, "Deze taak is ondertussen al afgerond.")
         return render(
             request,
@@ -644,11 +644,9 @@ def taak_afhandelen(request, id):
 
     if taaktypes:
         openstaande_taaktype_urls_voor_melding = [
-            taakopdracht.get("taaktype")
-            for taakopdracht in taak.melding.response_json.get(
-                "taakopdrachten_voor_melding", []
-            )
-            if taakopdracht.get("status", {}).get("naam") == "nieuw"
+            to.get("taaktype")
+            for to in taak.melding.response_json.get("taakopdrachten_voor_melding", [])
+            if to.get("status", {}).get("naam") == "nieuw"
         ]
         alle_volgende_taaktypes = [
             (
