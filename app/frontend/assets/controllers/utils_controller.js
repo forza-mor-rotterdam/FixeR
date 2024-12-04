967fac54c2746a0b9db44343514e07b8da9bdc59
Remco Hoff <remco@code010.nl>
2024-12-04 14:50:10 +0100

    Making it all work

--- app/frontend/assets/controllers/utils_controller.js
+++ app/frontend/assets/controllers/utils_controller.js
@@ -5,7 +5,20 @@ export default class extends Controller {
 
   initialize() {}
 
-  connect() {}
+  connect() {
+    if ('ontouchstart' in document.documentElement) {
+      document.body.classList.add('hasTouch')
+    }
+    window.addEventListener('click', (e) => {
+      if (
+        !(e.target.closest('.container__uitklapper') || e.target.hasAttribute('data-action')) || // data-action conflicts with window.evenlistener
+        e.target.classList.contains('btn-close--small')
+      ) {
+        this.noHover()
+        document.querySelector('.container__uitklapper').classList.remove('show')
+      }
+    })
+  }
 
   goToUrl(e) {
     window.location.href = e.params.url
@@ -13,5 +26,21 @@ export default class extends Controller {
 
   foldOut() {
     this.element.classList.toggle('show')
+  }
+  show() {
+    if ('ontouchstart' in document.documentElement) {
+      this.element.classList.add('show')
+    }
+  }
+
+  noHover(e) {
+    // used to close popup with close-button while hovering
+    const targetContainer = e?.params?.targetcontainer ?? 'container__uitklapper'
+    this.element.closest(`.${targetContainer}`).classList.remove('show')
+    this.element.closest(`.${targetContainer}`).classList.add('no-hover')
+    this.element.blur()
+    setTimeout(() => {
+      this.element.closest(`.${targetContainer}`).classList.remove('no-hover')
+    }, 500)
   }
 }
