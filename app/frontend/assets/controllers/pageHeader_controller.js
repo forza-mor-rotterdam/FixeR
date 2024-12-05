5e652bde1aaafbf0ab3c6b61d471b1428b3d5379
Remco Hoff <remco@code010.nl>
2024-12-05 15:38:48 +0100

    Cleanup code

--- app/frontend/assets/controllers/pageHeader_controller.js
+++ app/frontend/assets/controllers/pageHeader_controller.js
@@ -2,6 +2,17 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   connect() {
+    window.addEventListener('click', (e) => {
+      if (
+        !(e.target.closest('.container__uitklapper') || e.target.hasAttribute('data-action')) || // data-action conflicts with window.evenlistener
+        e.target.classList.contains('btn-close--small')
+      ) {
+        this.element.querySelectorAll('.show').forEach((element) => {
+          element.classList.remove('show')
+        })
+      }
+    })
+
     document.addEventListener('keydown', function (e) {
       if (e.code == 'Space' || e.code == 'Enter') {
         document.activeElement.click()
--- app/frontend/assets/controllers/utils_controller.js
+++ app/frontend/assets/controllers/utils_controller.js
@@ -1,38 +1,11 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  // static params = ["url"]
-
-  initialize() {}
-
-  connect() {
-    window.addEventListener('click', (e) => {
-      if (
-        !(e.target.closest('.container__uitklapper') || e.target.hasAttribute('data-action')) || // data-action conflicts with window.evenlistener
-        e.target.classList.contains('btn-close--small')
-      ) {
-        this.noHover()
-        document.querySelector('.container__uitklapper').classList.remove('show')
-      }
-    })
-  }
-
   goToUrl(e) {
     window.location.href = e.params.url
   }
 
   foldOut() {
     this.element.classList.toggle('show')
-  }
-
-  noHover(e) {
-    // used to close popup with close-button while hovering
-    const targetContainer = e?.params?.targetcontainer ?? 'container__uitklapper'
-    this.element.closest(`.${targetContainer}`).classList.remove('show')
-    this.element.closest(`.${targetContainer}`).classList.add('no-hover')
-    this.element.blur()
-    setTimeout(() => {
-      this.element.closest(`.${targetContainer}`).classList.remove('no-hover')
-    }, 500)
   }
 }
