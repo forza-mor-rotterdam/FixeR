d64cdc6a1c3fc331826031d43a87c3643c578836
Remco Hoff <remco@code010.nl>
2024-12-09 16:05:04 +0100

    toggle notificationcenter, z-indexes, hide snack on center open

--- app/frontend/assets/controllers/pageHeader_controller.js
+++ app/frontend/assets/controllers/pageHeader_controller.js
@@ -7,10 +7,14 @@ export default class extends Controller {
         !(e.target.closest('.container__uitklapper') || e.target.hasAttribute('data-action')) || // data-action conflicts with window.evenlistener
         e.target.classList.contains('btn-close--small')
       ) {
-        this.element.querySelectorAll('.show').forEach((element) => {
+        this.element.querySelectorAll('.container__uitklapper.show').forEach((element) => {
           element.classList.remove('show')
         })
       }
+      document.body.classList.toggle(
+        'show--notificatie-lijst',
+        this.element.querySelector('.show .container__profiel_notificatie_lijst')
+      )
     })
 
     document.addEventListener('keydown', function (e) {
@@ -34,12 +38,20 @@ export default class extends Controller {
 
   show(e) {
     if (e.target.tagName != 'A') {
-      this.element.querySelectorAll('.show').forEach((element) => {
-        if (element != e.target.closest('.container__uitklapper')) {
-          element.classList.remove('show')
-        }
-      })
-      e.target.closest('.container__uitklapper').classList.add('show')
+      if (this.element.querySelectorAll('.show').length) {
+        this.element.querySelectorAll('.show').forEach((element) => {
+          if (element != e.target.closest('.container__uitklapper')) {
+            element.classList.remove('show')
+          }
+          e.target.closest('.container__uitklapper').classList.toggle('show')
+        })
+      } else {
+        e.target.closest('.container__uitklapper').classList.add('show')
+      }
     }
+    document.body.classList.toggle(
+      'show--notificatie-lijst',
+      this.element.querySelector('.show .container__profiel_notificatie_lijst')
+    )
   }
 }
