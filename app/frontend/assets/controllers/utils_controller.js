7ba29e98f8eae9d557fa9a83f232976c8a8f8f20
Remco Hoff <remco@code010.nl>
2025-04-14 15:39:38 +0200

    Debug scriptje schermbreedte

--- app/frontend/assets/controllers/utils_controller.js
+++ app/frontend/assets/controllers/utils_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = ['breedte']
+
+  breedteTargetConnected(elem) {
+    elem.textContent = window.innerWidth
+  }
+
   goToUrl(e) {
     window.location.href = e.params.url
   }
