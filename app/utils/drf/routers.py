37895094093a941c18e4ac55e38f66c784c013f9
Maurice Guikema <maurice@mgui.nl>
2025-10-30 11:29:08 +0100

    reverse fix trailing slash

--- app/utils/drf/routers.py
+++ app/utils/drf/routers.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 
 class OptionalSlashRouter(DefaultRouter):
-    def __init__(self):
-        super().__init__()
-        self.trailing_slash = "/?"
+    ...
+    # def __init__(self):
+    #     super().__init__()
+    #     self.trailing_slash = "/?"
