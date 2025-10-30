acbd8c80abe0c3221fb75aaed75b51c1d14b5c3b
Maurice Guikema <maurice@mgui.nl>
2025-10-30 17:56:45 +0100

    taakgebeurtenis sync to MORCore problems to metrics

--- app/apps/main/metrics_collectors.py
+++ app/apps/main/metrics_collectors.py
@@ -12,6 +12,7 @@ class CustomCollector(object):
 
     def collect(self):
         yield self.collect_meldingalias_data_update_issues_metrics()
+        yield self.collect_taakgebeurtenis_notificatie_issues_metrics()
 
     def collect_meldingalias_data_update_issues_metrics(self):
         c = CounterMetricFamily(
@@ -37,5 +38,34 @@ class CustomCollector(object):
             (),
             results[0].get("count") if results else 0,
         )
+        return c
 
+    def collect_taakgebeurtenis_notificatie_issues_metrics(self):
+        c = CounterMetricFamily(
+            "fixer_taakgebeurtenis_notificatie_issues_total",
+            "De aantallen van Taakgebeurtenissen met problemen met versturen van de notificatie naar MORCore",
+            labels=[],
+        )
+        results = []
+
+        sql = 'SELECT \
+            COUNT(*) AS "count" \
+            FROM "taken_taakgebeurtenis" \
+                LEFT OUTER JOIN "django_celery_results_taskresult" ON ("taken_taakgebeurtenis"."task_taakopdracht_notificatie_id" = "django_celery_results_taskresult"."id") \
+                JOIN "taken_taakstatus" ON ("taken_taakgebeurtenis"."taakstatus_id" = "taken_taakstatus"."id") \
+            WHERE  \
+                "taken_taakgebeurtenis"."notificatie_verstuurd" IS FALSE  \
+                AND "taken_taakstatus"."naam" = \'voltooid\'  \
+                AND ("django_celery_results_taskresult"."status" = ANY(ARRAY[\'FAILURE\', \'SUCCESS\'])  \
+                OR "taken_taakgebeurtenis"."task_taakopdracht_notificatie_id" IS NULL)  \
+        '
+
+        with connections["default"].cursor() as cursor:
+            cursor.execute(sql)
+            results = self.dictfetchall(cursor)
+
+        c.add_metric(
+            (),
+            results[0].get("count") if results else 0,
+        )
         return c
