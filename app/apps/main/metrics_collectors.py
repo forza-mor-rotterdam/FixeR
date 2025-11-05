179476af2d441e02024bf2f7296326879d4473b8
Maurice Guikema <maurice@mgui.nl>
2025-11-05 12:03:02 +0100

    metrics for Celery TaskResult issues

--- app/apps/main/metrics_collectors.py
+++ app/apps/main/metrics_collectors.py
@@ -69,3 +69,46 @@ class CustomCollector(object):
             results[0].get("count") if results else 0,
         )
         return c
+
+    def collect_celery_task_results(self):
+        c = CounterMetricFamily(
+            "morcore_celery_task_results_issues_total",
+            "Aantallen van Celery TaskResult instanties per task_name en status, met status FAILED of RETRY",
+            labels=[
+                "task_name",
+                "status",
+            ],
+        )
+        total_objects = []
+
+        sql = '\
+            SELECT \
+                "django_celery_results_taskresult"."task_name", \
+                "django_celery_results_taskresult"."status", \
+                COUNT("django_celery_results_taskresult"."task_id") AS "count" \
+            FROM "django_celery_results_taskresult" \
+            WHERE \
+                "django_celery_results_taskresult"."status" IN (\'FAILURE\', \'RETRY\') \
+                AND "django_celery_results_taskresult"."task_name" IS NOT NULL \
+            GROUP BY \
+                "django_celery_results_taskresult"."status", \
+                "django_celery_results_taskresult"."task_name" \
+            ORDER BY \
+                "django_celery_results_taskresult"."status", \
+                "django_celery_results_taskresult"."task_name" ASC;\
+        '
+
+        with connections["default"].cursor() as cursor:
+            cursor.execute(sql)
+            total_objects = self.dictfetchall(cursor)
+
+        for obj in total_objects:
+            c.add_metric(
+                (
+                    obj["task_name"],
+                    obj["status"],
+                ),
+                obj["count"],
+            )
+
+        return c
