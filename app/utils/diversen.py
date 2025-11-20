bed980bc8eebf5feb0f924a52c7435c3d086842a
Remco Hoff <remco@code010.nl>
2025-11-20 16:01:01 +0100

    Restyle header and filter-button and -dialog

--- app/utils/diversen.py
+++ app/utils/diversen.py
@@ -16,6 +16,23 @@ def gebruikersnaam(gebruiker):
     return gebruiker.email
 
 
+def gebruikersinitialen(gebruiker):
+    full_initials = "@"
+    if isinstance(gebruiker, dict):
+        first_name_initial = (
+            gebruiker.get("first_name", "@")[0]
+            if gebruiker.get("first_name", "@")
+            else "@"
+        )
+        # last_name_initial = gebruiker.get("last_name", "*")[0]
+        full_initials = f"{first_name_initial}".strip()
+    elif hasattr(gebruiker, "first_name") or hasattr(gebruiker, "last_name"):
+        first_name_initial = gebruiker.first_name[0] if gebruiker.first_name else "@"
+        # last_name_initial = gebruiker.last_name[0] if gebruiker.last_name else "*"
+        full_initials = f"{first_name_initial}".strip()
+    return full_initials
+
+
 def truncate_tekst(text, length=200):
     if len(text) > length:
         return f"{text[:length]}..."
